e32e4c084b103bab0bc05e24ae5f5deacfa3cba5
= <thaistakahara@hotmail.com>
2022-06-04 14:30:40 -0300

    Obtendo um usuario por id

--- src/main/java/com/thaistakahara/workshopmongo/services/UserServices.java
+++ src/main/java/com/thaistakahara/workshopmongo/services/UserServices.java
@@ -1,12 +1,14 @@
 package com.thaistakahara.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.thaistakahara.workshopmongo.domain.User;
 import com.thaistakahara.workshopmongo.repository.UserRepository;
+import com.thaistakahara.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserServices {
@@ -16,6 +18,11 @@ public class UserServices {
 
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));		
 	}
 	
 }
