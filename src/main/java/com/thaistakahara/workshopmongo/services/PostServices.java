3f46a003879add3c64b9a2eeda98e23234c63703
= <thaistakahara@hotmail.com>
2022-06-07 21:12:35 -0300

    Consulta simples com @query

--- src/main/java/com/thaistakahara/workshopmongo/services/PostServices.java
+++ src/main/java/com/thaistakahara/workshopmongo/services/PostServices.java
@@ -22,7 +22,8 @@ public class PostServices {
 	}	
 	
 	public List<Post> findByTitle(String text){
-		return repo.findByTitleContainingIgnoreCase(text);
+		return repo.searchTitle(text);
+		//return repo.findByTitleContainingIgnoreCase(text);
 	}
 	
 }
